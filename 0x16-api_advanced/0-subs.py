65056e889e8121d3328b39263da49c3196fe124a
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-04 19:18:36 +0000

    Hotfix

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" How many subs? """
+
+
+def number_of_subscribers(subreddit):
+    """ Returns subscriber count of subreddit or 0 """
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
+    return 0
--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,59 +1,61 @@
 #!/usr/bin/python3
+""" Count it! """
+from requests import get
 
-import operator
-import requests
-
-def do_request(subreddit, after):
-    """ Do request to endopoint"""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'
-    userA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0)\
-            Gecko/20100101 Firefox/76.0"
-
-    hdr = {"User-Agent": userA}
-    response = requests.get(url.format(subreddit, after), headers=hdr)
-    return response
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'my-app/0.0.1'}
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    """ do recursion while there are a next page of
-        posts
+def count_words(subreddit, word_list, after="", word_dic={}):
     """
-    res = do_request(subreddit, after)
-    if res.status_code != 200:
-        return None
-
-    res = res.json()
-    after = res['data']['after']
-    for post in res['data']['children']:
-        hot_list.append(post['data']['title'])
+    Returns a list containing the titles of all hot articles for a
+    given subreddit. If no results are found for the given subreddit,
+    the function should return None.
+    """
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
 
     if after is None:
-        return hot_list
-    return recurse(subreddit, hot_list, after)
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+        return None
 
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
 
-def count_words(subreddit, word_list, i=0, hot_list=[], res={}):
-    
-    after = ''
-    subs = word_list[i].split(',')[0]
-    patterns = []
-    if len(hot_list) == 0:
-        hot_list = recurse(subreddit, hot_list, after)
+    params = {
+        'limit': 100,
+        'after': after
+    }
 
-    for el in hot_list:
-        split_line = el.split(' ')
-        for w in split_line:
-            if subs == w or subs.capitalize() == w:
-                patterns.append(w)
+    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
 
-    res[subs] = len(patterns)
-    i += 1
-    
-    if i != len(word_list):
-        count_words(subreddit, word_list, i, hot_list, res)
-    else:
-        sort_res = {}
-        sort_res = dict(sorted(res.items(), key=operator.itemgetter(1), reverse=True))
-        for k,v in  sort_res.items():
-            if v > 0:
-                print('{}: {}'.format(k, v))
+    if r.status_code != 200:
+        return None
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+
+    except:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
